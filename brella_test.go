10494a6ea79d98059e438e2b4e051a491e5d68e5
Andreas Heinze <soulsplit@gmail.com>
2021-02-07 01:13:23 +0100

    added base unittest file

---
+++ brella_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"reflect"
+	"testing"
+
+	goex "github.com/nntaoli-project/goex"
+)
+
+func Test_getCredentials(t *testing.T) {
+	tests := []struct {
+		name string
+		want credentials
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getCredentials(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getCredentials() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getAPIHandle(t *testing.T) {
+	tests := []struct {
+		name string
+		want goex.API
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getAPIHandle(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getAPIHandle() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
