bc5948f704100be09a4f6da63374ec99355ecac1
Andreas Heinze <soulsplit@gmail.com>
2021-02-07 01:14:33 +0100

    added change calculation, reorganized code

--- brella.go
+++ brella.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 	"time"
 
@@ -15,6 +16,34 @@ import (
 	"github.com/nntaoli-project/goex/builder"
 	"github.com/olekukonko/tablewriter"
 )
+
+type matrix [][]string
+
+type credentials struct {
+	APIKey        string ""
+	APISecretkey  string ""
+	APIPassphrase string ""
+}
+type history struct {
+	currName string ""
+	value    []float64
+}
+
+type valuesMap map[string][]float64
+
+func (s matrix) Len() int {
+	return len(s)
+}
+func (s matrix) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s matrix) Less(i, j int) bool {
+	return s[i][0] < s[j][0]
+}
+
+func getVersion() {
+	fmt.Println("0.0.1")
+}
 
 func estimateName(name string) string {
 
@@ -32,68 +61,16 @@ func estimateName(name string) string {
 	return newName
 }
 
-type matrix [][]string
-
-func (s matrix) Len() int {
-	return len(s)
-}
-func (s matrix) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-func (s matrix) Less(i, j int) bool {
-	return s[i][0] < s[j][0]
-}
-
-func main() {
-	var currency string
-	var once bool
-
-	// flags declaration using flag package
-	flag.StringVar(&currency, "c", "EUR", "Specify the FIAT currency to take as a baseline. Default is euro")
-	flag.BoolVar(&once, "o", false, "Specify if the application should not keep running and give a new update every minute but run just once and quit. Default is false.")
-
-	flag.Parse() // after declaring flags we need to call it
-
-	apiGoex := getAPIHandle()
-	var FIAT = goex.Currency{Symbol: currency, Desc: ""}
-
-	for {
-		log.Println(fmt.Sprintf("Getting new data from %s", apiGoex.GetExchangeName()))
-		acc, _ := apiGoex.GetAccount()
-		var holdings [][]string
-		var sum float64
-		var value float64
-
-		for _, subacc := range acc.SubAccounts {
-			// skip small amounts, skip euro amount, skip redundant XBT as BTC is already reported for Bitcoin
-			if subacc.Amount < 0.001 ||
-				subacc.Currency == goex.XBT {
-				continue
-			}
-			value, holdings = extractHoldings(subacc, FIAT, value, holdings, apiGoex)
-			sum += value
-		}
-
-		printTable(FIAT, holdings, sum)
-		if once {
-			break
-		}
-		time.Sleep(60 * time.Second)
-		fmt.Println()
-	}
-
-}
-
-func extractHoldings(subacc goex.SubAccount, FIAT goex.Currency, value float64, holdings [][]string, apiGoex goex.API) (float64, [][]string) {
+func extractHoldings(subacc goex.SubAccount, FIAT goex.Currency, value float64, holdings [][]string, vMap valuesMap, apiGoex goex.API) (float64, [][]string) {
 	if subacc.Currency == FIAT {
 		value = subacc.Amount
-		holdings = addHoldings(holdings, FIAT.String(), 0, subacc.Amount, value)
+		holdings = addHoldings(holdings, FIAT.String(), 0, subacc.Amount, value, vMap)
 	} else {
 		currName := estimateName(subacc.Currency.String())
 		pair := getPair(currName, FIAT)
 		price := getPrice(apiGoex, pair)
 		value = subacc.Amount * price.Last
-		holdings = addHoldings(holdings, currName, subacc.Amount, price, value)
+		holdings = addHoldings(holdings, currName, subacc.Amount, price, value, vMap)
 	}
 	return value, holdings
 }
@@ -105,13 +82,26 @@ func printTable(FIAT goex.Currency, holdings [][]string, sum float64) {
 		"Name",
 		"Amount",
 		fmt.Sprintf("Price (%s)", FIAT),
-		fmt.Sprintf("Value (%s)", FIAT)})
+		fmt.Sprintf("Value (%s)", FIAT),
+		"Start (%)",
+		"Last (%)"},
+	)
+	table.SetColumnAlignment([]int{
+		tablewriter.ALIGN_DEFAULT,
+		tablewriter.ALIGN_DEFAULT,
+		tablewriter.ALIGN_DEFAULT,
+		tablewriter.ALIGN_DEFAULT,
+		tablewriter.ALIGN_RIGHT,
+		tablewriter.ALIGN_RIGHT})
 
 	table.SetFooter([]string{
 		fmt.Sprintf("∑ %d", len(holdings)),
 		" ",
 		" ",
-		fmt.Sprintf("%.2f", sum)})
+		fmt.Sprintf("%.2f", sum),
+		" ",
+		" "})
+
 	table.SetFooterAlignment(tablewriter.ALIGN_RIGHT)
 
 	table.SetBorder(false)
@@ -135,8 +125,18 @@ func getPair(currName string, FIAT goex.Currency) goex.CurrencyPair {
 	return pair
 }
 
-func addHoldings(holdings [][]string, currName string, amount float64, price interface{}, value float64) [][]string {
+func addHoldings(holdings [][]string, currName string, amount float64, price interface{}, value float64, vMap valuesMap) [][]string {
 	var priceF float64
+	var changeSinceLast []string
+
+	vMap = storeValues(currName, value, vMap)
+	oldValue := vMap[currName][0]
+	changeSinceStart := getPercentage(value, oldValue, vMap, currName)
+
+	if len(vMap[currName]) > 1 {
+		recentValue := vMap[currName][len(vMap[currName])-2]
+		changeSinceLast = getPercentage(value, recentValue, vMap, currName)
+	}
 
 	switch v := price.(type) {
 	case float64:
@@ -146,12 +146,32 @@ func addHoldings(holdings [][]string, currName string, amount float64, price int
 	default:
 		fmt.Println("don't know the type")
 	}
+
 	holdings = append(holdings,
 		[]string{fmt.Sprintf("%s", currName),
 			fmt.Sprintf("%.4f", amount),
 			fmt.Sprintf("%.2f", priceF),
-			fmt.Sprintf("%.2f", value)})
+			fmt.Sprintf("%.2f", value),
+			strings.Join(changeSinceStart, ""),
+			strings.Join(changeSinceLast, "")})
 	return holdings
+}
+
+func getPercentage(value float64, oldValue float64, vMap valuesMap, currName string) []string {
+	change, setColor := calcChange(value, oldValue, vMap, currName)
+	changeSinceValue := colorize(setColor, strconv.FormatFloat(change, 'f', 2, 64))
+	return changeSinceValue
+}
+
+func calcChange(value float64, oldValue float64, vMap valuesMap, currName string) (float64, string) {
+	change := (value/oldValue - 1) * 100
+	setColor := "green"
+	if change < 0.0 {
+		setColor = "red"
+	} else if change == 0.0 {
+		setColor = "default"
+	}
+	return change, setColor
 }
 
 func getAPIHandle() goex.API {
@@ -165,10 +185,31 @@ func getAPIHandle() goex.API {
 	return apiGoex
 }
 
-type credentials struct {
-	APIKey        string ""
-	APISecretkey  string ""
-	APIPassphrase string ""
+func colorize(color string, content string) []string {
+	colorReset := "\033[0m"
+	colorRed := "\033[31m"
+	colorGreen := "\033[32m"
+	var setting string
+	switch color {
+	case "red":
+		setting = string(colorRed)
+	case "green":
+		setting = string(colorGreen)
+	default:
+		setting = string(colorReset)
+	}
+	return []string{setting, content, string(colorReset)}
+
+}
+
+func storeValues(currName string, value float64, vMap valuesMap) valuesMap {
+	_, ok := vMap[currName]
+	if !ok {
+		vMap[currName] = []float64{value}
+	} else {
+		vMap[currName] = append(vMap[currName], value)
+	}
+	return vMap
 }
 
 func getCredentials() credentials {
@@ -209,3 +250,50 @@ func getCredentials() credentials {
 // 	log.Printf("%f", data.AvgPrice)
 // }
 // }
+
+func main() {
+	var currency string
+	var once bool
+	var version bool
+
+	// flags declaration using flag package
+	flag.StringVar(&currency, "c", "EUR", "Specify the FIAT currency to take as a baseline.")
+	flag.BoolVar(&once, "o", false, "Specify if the application should not keep running and give a new update every minute but run just once and quit.")
+	flag.BoolVar(&version, "v", false, "Specify if the application should print the version and quit.")
+
+	flag.Parse() // after declaring flags we need to call it
+	if version {
+		getVersion()
+		return
+	}
+
+	apiGoex := getAPIHandle()
+	var FIAT = goex.Currency{Symbol: currency, Desc: ""}
+	var vMap = make(valuesMap)
+
+	for {
+		log.Println(fmt.Sprintf("Getting new data from %s", apiGoex.GetExchangeName()))
+		acc, _ := apiGoex.GetAccount()
+		var holdings [][]string
+		var sum float64
+		var value float64
+
+		for _, subacc := range acc.SubAccounts {
+			// skip small amounts, skip euro amount, skip redundant XBT as BTC is already reported for Bitcoin
+			if subacc.Amount < 0.001 ||
+				subacc.Currency == goex.XBT {
+				continue
+			}
+			value, holdings = extractHoldings(subacc, FIAT, value, holdings, vMap, apiGoex)
+			sum += value
+		}
+
+		printTable(FIAT, holdings, sum)
+		if once {
+			break
+		}
+		time.Sleep(60 * time.Second)
+		fmt.Println()
+	}
+
+}
